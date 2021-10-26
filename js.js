bf9981fef8f782470fe2bc2185add663debce557
EBONYBARK461 <pipe13062007@gmail.com>
2021-10-26 13:28:45 -0500

    version 1

---
+++ js.js
@@ -0,0 +1,9 @@
+
+let array=['1', '2', '3', '1', '2', '3']
+let =['hola', 'como', 'estas']
+let conteo=array.length
+
+for (let index = 0; index < conteo; index++) {
+    document.write(array[index])
+
+}
