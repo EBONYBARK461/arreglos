edd217d4e78510c89bbc930ee9daf68bac07e30c
EBONYBARK461 <pipe13062007@gmail.com>
2021-10-26 14:52:09 -0500

    version 2

--- js.js
+++ js.js
@@ -1,9 +1,32 @@
 
-let array=['1', '2', '3', '1', '2', '3']
-let =['hola', 'como', 'estas']
+let array=['a', 'b', 'c', 'd', 'e', 'f']
 let conteo=array.length
 
 for (let index = 0; index < conteo; index++) {
     document.write(array[index])
 
 }
+
+
+
+let casa = [ 
+    {
+        "color_muebles":"morado",
+        "electrodomesticos":"minivan",
+        "compra": new Date('1978-01-03'),
+        "viven":7
+    },
+    {
+        "color_muebles":"rojo",
+        "electrodomesticos":"camioneta",
+        "compra": new Date('2010-03-03'),
+        "viven":5
+    }
+]
+let cobjetos=casa.length
+
+for (let index = 0; index < objetos; index++) {
+    document.write(casa[index].color_muebles+'<br>')
+    
+}
+
