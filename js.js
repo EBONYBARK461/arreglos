58d6bf76a2458d33f69294e2259fac69e5f42f33
EBONYBARK461 <pipe13062007@gmail.com>
2021-10-26 15:26:25 -0500

    version 3

--- js.js
+++ js.js
@@ -1,29 +1,35 @@
 
-let array=['a', 'b', 'c', 'd', 'e', 'f']
-let conteo=array.length
+let abecedario=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ']
+let conteo=abecedario.length
 
 for (let index = 0; index < conteo; index++) {
-    document.write(array[index])
+    document.write(abecedario[index])
 
 }
 
+let colores=['azul', 'verde', 'cafe', 'rosado', 'violeta', 'amarillo', 'negro', 'blanco', 'naranja', 'gris', 'morado', 'marron', 'cian', 'beach', 'fucsia']
+let cantidad=colores.length
 
+for (let index = 0; index < cantidad; index++) {
+    document.write(colores[index]+'<br>')
+
+}
 
 let casa = [ 
     {
-        "color_muebles":"morado",
-        "electrodomesticos":"minivan",
+        "color_muebles":"verde",
+        "electrodomesticos":"nevera",
         "compra": new Date('1978-01-03'),
         "viven":7
     },
     {
-        "color_muebles":"rojo",
-        "electrodomesticos":"camioneta",
+        "color_muebles":"azul",
+        "electrodomesticos":"",
         "compra": new Date('2010-03-03'),
         "viven":5
     }
 ]
-let cobjetos=casa.length
+let objetos=casa.length
 
 for (let index = 0; index < objetos; index++) {
     document.write(casa[index].color_muebles+'<br>')
